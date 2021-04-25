fc5da2ca344e3c19ae53b9f1d2327a5663ecaf70
Santiago Gonzalez <sagodi97@gmail.com>
2021-04-25 16:12:03 +0200

    Add deleteById and fix getAll in user DAO

--- src/dao/user.dao.js
+++ src/dao/user.dao.js
@@ -5,17 +5,29 @@ const { ObjectId } = db;
 
 const Users = MongoDB.client.db(dbName).collection('users');
 
-export const create = async (user) => {
-  await Users.insertOne(user);
-};
+export const create = async (user) => Users.insertOne(user);
 
 export const usernameExists = async (username) => !!(await Users.findOne({ username }));
 
 export const emailExists = async (email) => !!(await Users.findOne({ email }));
 
-export const getAll = async () => Users.find({});
+// OPTIMIZE: Unclear how this will behave with a large amount of documents in the collection
+export const getAll = async () => {
+  const cursor = await Users.find();
+  const users = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const user of cursor) {
+    users.push(user);
+  }
+  return users;
+};
 
 export const getById = async (userId) => {
   const id = ObjectId(userId);
   return Users.findOne({ _id: id });
+};
+
+export const deleteById = async (userId) => {
+  const id = ObjectId(userId);
+  return Users.deleteOne({ _id: id });
 };
