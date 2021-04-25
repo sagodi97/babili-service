762e715aeb64aba1f7a4f3695bb3c0b81f44f5d6
Santiago Gonzalez <sagodi97@gmail.com>
2021-04-25 20:44:20 +0200

    Omit returning password field in userDao: create, getAll, getById

--- src/dao/user.dao.js
+++ src/dao/user.dao.js
@@ -5,7 +5,11 @@ const { ObjectId } = db;
 
 const Users = MongoDB.client.db(dbName).collection('users');
 
-export const create = async (user) => Users.insertOne(user);
+export const create = async (user) => {
+  const createdUser = await Users.insertOne(user);
+  const { password, ...rest } = createdUser.ops[0];
+  return rest;
+};
 
 export const usernameExists = async (username) => !!(await Users.findOne({ username }));
 
@@ -13,7 +17,7 @@ export const emailExists = async (email) => !!(await Users.findOne({ email }));
 
 // OPTIMIZE: Unclear how this will behave with a large amount of documents in the collection
 export const getAll = async () => {
-  const cursor = await Users.find();
+  const cursor = await Users.find({}, { projection: { password: 0 } });
   const users = [];
   // eslint-disable-next-line no-restricted-syntax
   for await (const user of cursor) {
@@ -24,10 +28,12 @@ export const getAll = async () => {
 
 export const getById = async (userId) => {
   const id = ObjectId(userId);
-  return Users.findOne({ _id: id });
+  return Users.findOne({ _id: id }, { projection: { password: 0 } });
 };
 
 export const deleteById = async (userId) => {
   const id = ObjectId(userId);
   return Users.deleteOne({ _id: id });
 };
+
+export const getByEmail = async (email) => Users.findOne({ email });
